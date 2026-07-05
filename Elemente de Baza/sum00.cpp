dec640f0ad656de82a64ce4b94f5cc80c1c0e0b7
ArisNero <ArisHatter@gmail.com>
2026-07-05 02:43:26 -0700

    Elemente de Baza

--- Elemente de Baza/sum00.cpp
+++ Elemente de Baza/sum00.cpp
@@ -6,3 +6,5 @@ int main()
 	cin >> a >> b;s = a + b;
 	cout << s;
 }
+
+// the variables a and b get two numbers. the variable s receives the sum of a+b; long is used so that big numbers do not represent a problem. cout<<s is used to display the sum
