aed2b5dc0e54d6950a7070b652b1502c15b95e6e
ArisNero <ArisHatter@gmail.com>
2026-07-06 01:57:57 -0700

    Elemente de Baza

---
+++ Elemente de Baza/Globuri.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int a,s;
+int main()
+{
+	cin >> a;
+	s = a + a * 2 + (a*2-3);
+	cout << s;
+}
+//this code uses the variable s to store the total Christmas balls. 
+/*
+* this part of the code 
+* s = a + a * 2 + (a*2-3);
+	cout << s;
+
+	can be written 
+	cout << a + a * 2 + (a*2-3);
+
+
+*/
