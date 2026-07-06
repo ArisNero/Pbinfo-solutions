85390e9357ddcbfb7fae24bd1af6ff72a75a387d
ArisNero <ArisHatter@gmail.com>
2026-07-06 01:28:58 -0700

    Elemente de Baza

---
+++ Elemente de Baza/scadere2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+long a, b,dif;
+int main()
+{
+	cin >> a >> b;
+	dif = a - b;
+	cout << dif;
+	/*
+	* or, instead of using the variable dif, we can just say cout<< a-b; The result is the same
+	*/
+}
