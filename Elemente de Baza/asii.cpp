c726b094f81d1230d765c17022d6a6201956faa5
ArisNero <ArisHatter@gmail.com>
2026-07-06 01:36:19 -0700

    modifications

---
+++ Elemente de Baza/asii.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int a, b;
+int main()
+{
+	cin >> a >> b;
+	cout << a + b << " " << a - b << " " << a * b << " " << a / b;
+}
+//we use <<" "<< to put a space between the results. a+b is the sum, a-b is the subtraction, a*b is the multiplication and a/b is the division(the quotient)
