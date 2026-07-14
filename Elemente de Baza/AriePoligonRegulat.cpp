55c7fe8492c8895f301a26aaca174c4d22bd8633
ArisNero <ArisHatter@gmail.com>
2026-07-14 01:53:35 -0700

    Elemente de Baza

--- Elemente de Baza/AriePoligonRegulat.cpp
+++ Elemente de Baza/AriePoligonRegulat.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-#include <numbers>
 using namespace std;
 long n, l;
 double A;
 int main()
 {
 	cin >> n >> l;
-	A = (n * l*l*1) / (4 * tan( M_PI/ n));
+	A = (n *pow(l,2)) / (4 * tan( M_PI/ n));
 	cout << fixed << setprecision(2) << A;
 }
+// this solution is only 50 points ; we use #define _USE_MATH_DEFINES and the library <cmath> in roder to use the value of  pi
+ 
